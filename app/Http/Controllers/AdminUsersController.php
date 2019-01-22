f9d4a3ca47c71e7aad3a927da22f9e381729487b
Phuc <enzcoding@gmail.com>
2019-01-22 22:40:29 +0700

    Install laravelcollective and create Create Form

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -44,6 +44,7 @@ class AdminUsersController extends Controller
     public function store(Request $request)
     {
         //
+        return $request->all();
     }
 
     /**
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -7,6 +7,32 @@
 
     <h1>Create Users</h1>
 
+    {!! Form::open(['method'=>'POST', 'action'=> 'AdminUsersController@store']) !!}
+        <div class="form-group">
+            {!! Form::label('title', 'Title:') !!}
+            {!! Form::text('title', null, ['class'=>'form-control']) !!}
+            {{csrf_field()}}
+        </div>
+        <div class="form-group">
+            {!! Form::label('email', 'Email:') !!}
+            {!! Form::text('email', null, ['class'=>'form-control']) !!}
+            {{csrf_field()}}
+        </div>
+        <div class="form-group">
+            {!! Form::label('role_id', 'Role:') !!}
+            {!! Form::text('role_id', null, ['class'=>'form-control']) !!}
+            {{csrf_field()}}
+        </div>
+        <div class="form-group">
+            {!! Form::label('status', 'Status:') !!}
+            {!! Form::text('status', null, ['class'=>'form-control']) !!}
+            {{csrf_field()}}
+        </div>
+        <div class="form-group">
+             {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+        </div>
+    {!! Form::close() !!}
+
 
 
 
