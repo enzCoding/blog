51f9ae877733070127e17e467208e22f8a649c7f
Phuc <enzcoding@gmail.com>
2019-01-24 22:25:21 +0700

    Create Posts Form

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -38,9 +38,10 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostsCreateRequest $request)
     {
         //
+        return $request->all();
     }
 
     /**
