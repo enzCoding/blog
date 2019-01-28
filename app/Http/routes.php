344d66265533757ceee2e01bf72eb2e1f4fbbe4b
Phuc <enzcoding@gmail.com>
2019-01-28 23:08:32 +0700

    Admin Media CRUD done

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,5 +29,8 @@ Route::group(['middleware'=>'admin'], function (){
     Route::resource('admin/posts', 'AdminPostsController');
 
     Route::resource('admin/categories', 'AdminCategoriesController');
+
+    Route::resource('admin/media','AdminMediaController');
+
 });
 
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,7 +24,7 @@
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-
+    @yield('styles')
 
 
 </head>
@@ -169,11 +169,11 @@
                         <a href="#"><i class="fa fa-wrench fa-fw"></i>Media<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a href="/media">All Media</a>
+                                <a href="{{route('admin.media.index')}}">All Media</a>
                             </li>
 
                             <li>
-                                <a href="">Upload Media</a>
+                                <a href="{{route('admin.media.create')}}">Upload Media</a>
                             </li>
 
                         </ul>
@@ -330,7 +330,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Admin</h1>
+                <h1 class="page-header"></h1>
 
                 @yield('content')
             </div>
@@ -348,8 +348,7 @@
 <!-- jQuery -->
 <script src="{{asset('js/libs.js')}}"></script>
 
-
-@yield('footer')
+@yield('scripts')
 
 
 
