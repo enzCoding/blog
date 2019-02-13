094709442b416511b9c70e9d2cf262c658fe6f98
Phuc <enzcoding@gmail.com>
2019-02-13 10:08:21 +0700

    Approving, un-approving and deleting comments on admin page

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -91,6 +91,9 @@ class PostCommentsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        Comment::findOrFail($id)->update($request->all());
+
+        return redirect('/admin/comments');
     }
 
     /**
@@ -102,5 +105,8 @@ class PostCommentsController extends Controller
     public function destroy($id)
     {
         //
+        Comment::findOrFail($id)->delete();
+
+        return redirect()->back();
     }
 }
